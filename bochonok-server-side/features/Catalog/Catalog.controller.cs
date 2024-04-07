331e5910792c8d531767e5d4148bb3054b21f20e
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-04-07 20:09:46 +0300

    feat: catalog controller

---
+++ bochonok-server-side/features/Catalog/Catalog.controller.cs
@@ -0,0 +1,14 @@
+﻿using bochonok_server_side.controllers;
+using bochonok_server_side.interfaces;
+using bochonok_server_side.model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bochonok_server_side.features.Catalog;
+
+[ApiController]
+[Route("[controller]")]
+public class CatalogController : AppController<CatalogItem, CatalogItem>
+{
+    public CatalogController(IEntityService<CatalogItem> service): base(service)
+    { }
+}
