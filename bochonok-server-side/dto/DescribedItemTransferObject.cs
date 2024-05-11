7edf04d27a622c34353a51630714c80915c16a51
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-05-11 23:52:48 +0300

    refactor: fix typo

--- bochonok-server-side/dto/DescribedItemTransferObject.cs
+++ bochonok-server-side/dto/DescribedItemTransferObject.cs
@@ -3,6 +3,6 @@ namespace bochonok_server_side.dto;
 public class DescribedItemTransferObject
 {
     public string title { get; set; }
-    public string desciption { get; set; }
+    public string description { get; set; }
     public string imageB64 { get; set; }
 }
