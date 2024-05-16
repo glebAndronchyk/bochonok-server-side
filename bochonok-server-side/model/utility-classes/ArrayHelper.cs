899d9f52dc877ab99d4dc5e5cf5c1bea4d45914e
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-05-17 00:12:44 +0300

    feat: array helper

---
+++ bochonok-server-side/model/utility-classes/ArrayHelper.cs
@@ -0,0 +1,17 @@
+namespace bochonok_server_side.model.utility_classes;
+
+public class ArrayHelper
+{
+    public static T[,] Make2DArray<T>(T[] input, int height, int width)
+    {
+        T[,] output = new T[height, width];
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                output[i, j] = input[i * width + j];
+            }
+        }
+        return output;
+    }
+}
